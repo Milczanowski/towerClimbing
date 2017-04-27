a3e0d41147116a5ef304b6887d54be335109c449
Michał Milczanowski <michalmilczanowski@hotmail.com>
2017-04-27 15:34:00 +0200

    ball and something

---
+++ towerClimbing/Assets/Scripts/Ball.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Assets.Scripts
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(CircleCollider2D))]
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class Ball : MonoBehaviour
+    {
+        [SerializeField]
+        private Rigidbody2D rigidBody2D;
+
+        public Rigidbody2D Rigidbody2D
+        {
+            get { return rigidBody2D; }
+            protected set { rigidBody2D = value; }
+        }
+
+        private void OnMouseDown()
+        {
+            Rigidbody2D.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+        }
+    }
+}
